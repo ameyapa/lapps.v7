31dfaa7e4b8f198377a4a9e9072b6bf42711583d
AMEYA <69630417+ameyapa@users.noreply.github.com>
2024-10-06 22:11:59 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,10 +5,13 @@ AB=flatpak
 lunix=nala 
 aa=snap
 
+sudo apt update && sudo apt upgrade -y
+
 sudo apt install nala -y
 
-sudo $lunix install xserver-xorg
-sudo $lunix install kde-plasma-desktop
+
+sudo $lunix install xserver-xorg -y
+sudo $lunix install kde-plasma-desktop -y
 sudo $lunix update && sudo $lunix upgrade -y
 sudo $lunix install vlc -y
 sudo $lunix install git -y
@@ -17,29 +20,29 @@ sudo $lunix install vim -y
 sudo $lunix install chromium-browser -y
 sudo $lunix update 
 sudo $lunix install telegram-desktop -y
-sudo $lunix install steam -y
+#sudo $lunix install steam -y
 sudo $lunix install rpi-imager -y
 sudo $lunix install $AB -y
 sudo $lunix install gnome-software-plugin-$AB -y
+sudo $lunix install cheese
 
 sudo $AB remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 $AB install flathub -y org.kde.okular
-$AB install flathub -y com.spotify.Client
-$AB install flathub -y org.gnome.Cheese
-$AB install flathub -y com.microsoft.Edge
+#$AB install flathub -y com.spotify.Client
+#$AB install flathub -y org.gnome.Cheese
+#$AB install flathub -y com.microsoft.Edge
 $AB install flathub -y org.gnome.Boxes
 $AB install flathub -y io.gitlab.adhami3310.Impression
 $AB install flathub -y io.gitlab.o20.word
-$AB install flathub -y ro.go.hmlendea.DL-Desktop
-$AB install flathub -y com.wps.Office
+#$AB install flathub -y ro.go.hmlendea.DL-Desktop
+#$AB install flathub -y com.wps.Office
  
 sudo $abcd dist-upgrade -y
 
 echo  ubantu snaps
  
- sudo $abcd install snap
+sudo $abcd install snap
 sudo $aa install ultimate-media-downloader 
 sudo $aa install zoom-client
 
-
-
+sudo apt install lightdm
