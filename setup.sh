5a17de3080f365f3efe8c24030e21a559708aa11
AMEYA <69630417+ameyapa@users.noreply.github.com>
2024-10-23 13:49:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,22 +20,22 @@ sudo $lunix install vim -y
 sudo $lunix install chromium-browser -y
 sudo $lunix update 
 sudo $lunix install telegram-desktop -y
-#sudo $lunix install steam -y
+sudo $lunix install steam -y
 sudo $lunix install rpi-imager -y
 sudo $lunix install $AB -y
 sudo $lunix install gnome-software-plugin-$AB -y
 sudo $lunix install cheese -y
 
 sudo $AB remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-#$AB install flathub -y org.kde.okular
-#$AB install flathub -y com.spotify.Client
-#$AB install flathub -y org.gnome.Cheese
-#$AB install flathub -y com.microsoft.Edge
-#$AB install flathub -y org.gnome.Boxes
-#$AB install flathub -y io.gitlab.adhami3310.Impression
+$AB install flathub -y org.kde.okular
+$AB install flathub -y com.spotify.Client
+$AB install flathub -y org.gnome.Cheese
+$AB install flathub -y com.microsoft.Edge
+$AB install flathub -y org.gnome.Boxes
+$AB install flathub -y io.gitlab.adhami3310.Impression
 $AB install flathub -y io.gitlab.o20.word
-#$AB install flathub -y ro.go.hmlendea.DL-Desktop
-#$AB install flathub -y com.wps.Office
+$AB install flathub -y ro.go.hmlendea.DL-Desktop
+$AB install flathub -y com.wps.Office
  
 sudo $abcd dist-upgrade -y
 
